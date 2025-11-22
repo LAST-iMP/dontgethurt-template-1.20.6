8ef10f9e228e4bfd340ca6c811de9598f3ec604a
LAST-iMP <xiaoshibang@126.com>
2025-11-22 16:46:06 +0800

    init version 1.0.0.0

--- src/main/java/com/lastimp/dgh/source/client/eventHandler/ForgeClientEventHandler.java
+++ src/main/java/com/lastimp/dgh/source/client/eventHandler/ForgeClientEventHandler.java
@@ -29,12 +29,15 @@ package com.lastimp.dgh.source.client.eventHandler;
 
 import com.lastimp.dgh.DontGetHurt;
 import com.lastimp.dgh.api.enums.KeyPressedType;
+import com.lastimp.dgh.source.Register.ModCapabilities;
 import com.lastimp.dgh.source.client.hotkey.KeyBinding;
 import com.lastimp.dgh.network.message.MyKeyPressedData;
+import com.lastimp.dgh.source.core.player.PlayerHealthCapability;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.InputEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 
 @EventBusSubscriber(modid = DontGetHurt.MODID,value = Dist.CLIENT)
@@ -44,6 +47,16 @@ public class ForgeClientEventHandler {
     public static void onKeyInput(InputEvent.Key event) {
         if(KeyBinding.OPEN_MENU_KEY.consumeClick()){
             PacketDistributor.sendToServer(MyKeyPressedData.getInstance(KeyPressedType.KEY_HEALTH_MENU));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerCloned(PlayerEvent.Clone event) {
+        if (!event.isWasDeath()) {
+            var oldHealth = PlayerHealthCapability.get(event.getOriginal());
+            var newHealth = PlayerHealthCapability.get(event.getEntity());
+            newHealth.deserializeNBT(event.getEntity().registryAccess(), oldHealth.serializeNBT(event.getEntity().registryAccess()));
+            PlayerHealthCapability.set(event.getEntity(), newHealth);
         }
     }
 
